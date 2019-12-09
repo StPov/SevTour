4374faa4a5450c2a39cd3e58dfb343a270844864
Stanislav <staspovolotskiy@gmail.com>
2019-12-09 20:39:24 +0300

    Initial Commit

---
+++ SevTour/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SevTour
+//
+//  Created by Stanislav Povolotskiy on 09.12.2019.
+//  Copyright © 2019 Stanislav Povolotskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
